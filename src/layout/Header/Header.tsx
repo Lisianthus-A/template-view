3dc2741cf5bdd925ad5c733633d54a0481d890b2
Lisianthus-A <lisianthus.zeng@gmail.com>
2025-08-02 15:00:54 +0800

    Revert "refactor: remove PSD import functionality and related code"
    This reverts commit bfa92bc53c984a4ef677655aa2df39a72f286540.

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,11 +1,14 @@
 import styles from "./Header.module.scss";
 import { useState, useContext, useEffect } from "react";
+import Psd from "@webtoon/psd";
 import { canvasRef } from "@/store";
 import { Button, Icon, Tooltip, Toast } from "@/components";
 import { CanvasDataContext } from "@/App";
+import { ImageModel } from "@/models";
 import { shouldToast } from "@/utils";
 import EventBus from "@/utils/event";
 import type { ChangeEvent } from "react";
+import { Group, Layer } from "@webtoon/psd";
 
 function Header() {
   const canvasData = useContext(CanvasDataContext);
@@ -37,6 +40,169 @@ function Header() {
 
   const handleNew = () => {
     location.replace(location.origin);
+  };
+
+  const handleImportByPsd = async (evt: ChangeEvent<HTMLInputElement>) => {
+    const file = evt.target.files && evt.target.files[0];
+    if (file === null) {
+      return;
+    }
+    evt.target.value = "";
+
+    if (!file.name.endsWith(".psd")) {
+      return Toast.show("请选择正确的文件类型");
+    }
+
+    // item.layerFrame.layerProperties.textProperties
+    try {
+      const arrayBuffer = await file.arrayBuffer();
+      const result = Psd.parse(arrayBuffer);
+      // renderByLayers(result);
+      renderByGroups(result);
+    } catch (err) {
+      Toast.show("psd 解析失败");
+      console.log("catch psd error", err);
+    }
+  };
+
+  const shouldSkipLayer = (layer: Layer) => {
+    let shouldSkip = false;
+    if (layer.isHidden) {
+      shouldSkip = true;
+    }
+    if (layer.width > 3600 || layer.height > 3600) {
+      shouldSkip = true;
+    }
+
+    shouldSkip && console.log(`skip layer ${layer.name}`);
+
+    return shouldSkip;
+  };
+
+  // const renderByLayers = async (result: Psd) => {
+  //   if (!canvasRef.current) {
+  //     return;
+  //   }
+
+  //   const models = [];
+  //   for (let i = result.layers.length - 1; i >= 0; --i) {
+  //     const layer = result.layers[i];
+  //     const u8Array = await layer.composite(true, true);
+  //     const imageData = new ImageData(u8Array, layer.width, layer.height);
+  //     const model = await ImageModel.create({
+  //       imageUrl: "",
+  //       imageData,
+  //       x: layer.left,
+  //       y: layer.top,
+  //     });
+  //     models.push(model);
+  //   }
+  //   canvasRef.current.resize(result.width, result.height);
+  //   models.forEach((model) => {
+  //     canvasRef.current!.add(model);
+  //   });
+  // };
+
+  // psd 按组渲染
+  const renderByGroups = async (result: Psd) => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const nodes = result.children;
+
+    const bfs = (group: Group) => {
+      const layers: Layer[] = [];
+      const groups: Group[] = [];
+
+      for (let i = group.children.length - 1; i >= 0; --i) {
+        const item = group.children[i];
+        if (item.type === "Layer") {
+          !shouldSkipLayer(item) && layers.push(item);
+        } else if (item.type === "Group") {
+          groups.push(item);
+        }
+      }
+
+      groups.forEach((item) => {
+        layers.push(...bfs(item));
+      });
+
+      return layers;
+    };
+
+    const layersList: Layer[][] = [];
+    for (let i = nodes.length - 1; i >= 0; --i) {
+      const node = nodes[i];
+      // @ts-ignore
+      if (node?.layerFrame?.layerProperties?.hidden) {
+        continue;
+      }
+
+      if (node.type === "Group") {
+        layersList.push(bfs(node));
+      } else if (node.type === "Layer") {
+        !shouldSkipLayer(node) && layersList.push([node]);
+      }
+    }
+
+    const models = [];
+    for (let i = 0; i < layersList.length; ++i) {
+      const layers = layersList[i];
+      if (layers.length === 0) {
+        continue;
+      }
+
+      let minX = layers[0].left,
+        minY = layers[0].top;
+
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d")!;
+      canvas.width = result.width;
+      canvas.height = result.height;
+      const canvas2 = document.createElement("canvas");
+      const ctx2 = canvas2.getContext("2d")!;
+
+      for (let j = 0; j < layers.length; ++j) {
+        const layer = layers[j];
+        const u8Array = await layer.composite(true, true);
+        const imageData = new ImageData(u8Array, layer.width, layer.height);
+        ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        canvas2.width = layer.width;
+        canvas2.height = layer.height;
+        ctx2.putImageData(imageData, 0, 0);
+        ctx.drawImage(canvas2, layer.left, layer.top);
+        minX = Math.min(minX, layer.left);
+        minY = Math.min(minY, layer.top);
+      }
+
+      ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+      canvas2.width = result.width - minX;
+      canvas2.height = result.height - minY;
+      ctx2.drawImage(
+        canvas,
+        minX,
+        minY,
+        canvas2.width,
+        canvas2.height,
+        0,
+        0,
+        canvas2.width,
+        canvas2.height
+      );
+
+      const model = await ImageModel.create({
+        imageUrl: canvas2.toDataURL(),
+        x: minX,
+        y: minY,
+      });
+      models.push(model);
+    }
+
+    canvasRef.current.resize(result.width, result.height);
+    models.forEach((model) => {
+      canvasRef.current!.add(model);
+    });
   };
 
   const handleImportByJson = async (evt: ChangeEvent<HTMLInputElement>) => {
@@ -136,7 +302,16 @@ function Header() {
           导出数据
         </Button>
 
-        <Button>
+        <Button style={{ marginRight: 8 }}>
+          导入设计图
+          <input
+            className="hide-input"
+            type="file"
+            accept=".psd"
+            onChange={handleImportByPsd}
+          />
+        </Button>
+        <Button style={{ marginRight: 8 }}>
           导入数据
           <input
             className="hide-input"
